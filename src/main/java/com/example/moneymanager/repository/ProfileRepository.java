ba1f8cc5f6342968aff2851ad35fc1f501018bd4
Rohitnkam <Rohitnkam15@gmail.com>
2025-09-26 23:17:36 +0530

    first commit

---
+++ src/main/java/com/example/moneymanager/repository/ProfileRepository.java
@@ -0,0 +1,16 @@
+package com.example.moneymanager.repository;
+
+import com.example.moneymanager.entity.ProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
+
+    //select * from tbl_profiles where email = ?
+    Optional<ProfileEntity> findByEmail(String email);
+
+    //select * from tbl_profiles where activation_token = ?
+    Optional<ProfileEntity> findByActivationToken(String activationToken);
+}
